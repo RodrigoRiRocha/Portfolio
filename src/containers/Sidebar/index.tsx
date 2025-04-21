5be467d9bc37f3139e86180de21fb66e02c35605
Rodrigo <rstuteu@gmail.com>
2025-04-21 18:08:48 -0300

    construindo a estrtutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import Titulo from '../../components/titulo'
+
+/* eslint-disable prettier/prettier */
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/RodrigoRiRocha.png" />
+    <Titulo fontSize={20}>Rodrigo</Titulo>
+    
+  </aside>
+)
+
+export default Sidebar
